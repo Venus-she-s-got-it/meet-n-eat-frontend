dc097d65aafe2e71d4a82f24053bb906d0848ab7
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-05 15:31:51 -0700

    Adds context wrapper in App.js that was lost in merge to dev

--- src/App.js
+++ src/App.js
@@ -20,16 +20,18 @@ function App() {
         <NavBar />
       </header>
       <main>
-        <Routes>
-          <Route path="*" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/users/authentication" element={<LogInSignUp />} />
-          <Route path="/results/:searchString" element={<SearchResults />} />
-          <Route path="/profile" element={<MyProfile />} />
-          <Route path="/restaurants/:restaurantId" element={<RestaurantDetail />} />
-          <Route path="/message-center" element={<MessageCenter />} />
-          <Route path="/faq" element={<FAQ />} />
-        </Routes>
+        <UrlContext.Provider value={url}>
+          <Routes>
+            <Route path="*" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/users/authentication" element={<LogInSignUp />} />
+            <Route path="/results/:searchString" element={<SearchResults />} />
+            <Route path="/profile" element={<MyProfile />} />
+            <Route path="/restaurants/:restaurantId" element={<RestaurantDetail />} />
+            <Route path="/message-center" element={<MessageCenter />} />
+            <Route path="/faq" element={<FAQ />} />
+          </Routes>
+        </UrlContext.Provider>
       </main>
     
     </div>

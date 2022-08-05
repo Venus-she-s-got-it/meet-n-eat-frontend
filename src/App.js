3b401d1fa690c37f48bfb0bf039a132a7c02317e
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-05 13:45:53 -0700

    Adds routes to App.js
    Co-Authored-By: David Sandoval <82617473+DavidJoao@users.noreply.github.com>
    Co-Authored-By: Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
    Co-Authored-By: hanswatkins <hanswatkins@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import { Routes, Route, Navigate } from 'react-router-dom'
+import FAQ from './pages/FAQ';
+import Home from './pages/Home';
+import LogInSignUp from './pages/LogInSignUp';
+import MessageCenter from './pages/MessageCenter';
+import MyProfile from './pages/MyProfile';
+import NavBar from './components/NavBar';
+import RestaurantDetail from './pages/RestaurantDetail';
+import SearchResults from './pages/SearchResults';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="*" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/users/authentication" element={<LogInSignUp />} />
+          <Route path="/results/:searchString" element={<SearchResults />} />
+          <Route path="/profile" element={<MyProfile />} />
+          <Route path="/results/:restaurantId" element={<RestaurantDetail />} />
+          <Route path="/message-center" element={<MessageCenter />} />
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </main>
     
     </div>
   );

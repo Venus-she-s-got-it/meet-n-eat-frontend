cb97d9bd1d4bcc89acbef4805acdb3748337beb5
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-06 10:29:46 -0700

    adds restaurant data to restaurantCard.js

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -3,13 +3,15 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { UrlContext } from '../App';
 import RestaurantCard from '../components/RestaurantCard'
+import { set } from 'mongoose';
 
 
 const SearchResults = () => {
     const url = useContext(UrlContext)
     const { searchString } = useParams()
     const [restaurantData, setRestaurantData] = useState(null) 
-    // const [userData, setUserData] = useState(null)
+    const [likedRestaurants, setLikedRestaurants] = useState()
+    const [usersLikes, setUsersLikes] = useState()
 
 
     useEffect(() => {
@@ -27,13 +29,32 @@ const SearchResults = () => {
                 console.log(data)
             }) 
         }, [])
-    return (
-        <div>
-           {restaurantData 
-           ? restaurantData.map(restaurant => <RestaurantCard restaurant={restaurant}/>)
-          : <p>No matches found</p>} 
-        </div>
-    )
+
+        // to update users likedrestaurants onClick
+        useEffect(() => {
+        // const likes = { likedrestaurants: {...likedrestaurants, }}
+        axios.put(`${url}/users/:id`)
+            .then((res, err) => {
+                if (res.status === 404) {
+                    console.log(err)
+                } else if (res.status === 200 || res.status === 304) {
+                    return res.data
+                }
+            })
+            .then((data) => {
+                setLikedRestaurants()
+            })
+        }, [])
+    if (restaurantData) {
+        return (
+            <div>
+                <RestaurantCard restaurant={restaurantData} />
+            {/* {restaurantData 
+            ? restaurantData.map(restaurant => <RestaurantCard restaurant={restaurant}/>)
+            : <p>No matches found</p>}  */}
+            </div>
+        )
+    }
 }
 
 export default SearchResults

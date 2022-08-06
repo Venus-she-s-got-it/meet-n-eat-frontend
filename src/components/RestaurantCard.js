3a403d22778c5d2cfba2d1b3eda283be7727bdd0
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-05 18:16:55 -0700

    adds to searchResults.js and restaurantCard.js
    fetches data from restaurant db in searchResults.js and passes data through to restaurantCard.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 
-const RestaurantCard = () => {
 
+const RestaurantCard = ({ restaurant }) => {
+const { name, image_url, display_phone, location.city, location.state, display_phone, price  } = restaurant
+
+// const categories = restaurant.categories
+// 
 return (
-    <div>Restaurant Card</div>
+    <div>
+    <Card>
+        <Card.Title>{name}</Card.Title>  
+        <Card.Subtitle></Card.Subtitle>
+        <Image 
+            src={image_url}
+            alt="restaurant-image"
+            width={100}
+            height={100}
+            style={{ marginRight: "5em", marginTop: ".25em"}}/>
+        <Card.Body>
+            <p>{display_phone}</p>
+            <p>{location.city}, {location.state}</p>
+            <p>{price}</p>
+        </Card.Body>
+    </Card>
+    </div>
 )
 }
 

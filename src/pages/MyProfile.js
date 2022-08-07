7f0ec05f398ec832065a0227edb72e6f9b1ff503
DavidJoao <davidsandoval596@gmail.com>
2022-08-07 09:45:20 -0700

    added seeds for oakland and sacramento

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -26,7 +26,7 @@ useEffect(() => {
 
 return (
     <div style={{margin:'4%', height:'85vh', display:'flex', flexDirection:'row' }} className='my-profile'>
-        <div style={{ border:'1px solid black', width:'30%', marginRight:'3%', display:'flex', justifyContent:'center'}} className='info-section'>
+        <div style={{ border:'1px solid black', width:'30%', marginRight:'3%', display:'flex', justifyContent:'center', borderRadius:'10px'}} className='info-section'>
             <Card style={{ width: '90%', padding:'3%' }}>
                 <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
                 <Card.Body>
@@ -44,7 +44,7 @@ return (
             {/* PROFILE INFORMATION CARD COMPONENT  */}
         </div>
         <div style={{width:'45%', marginRight:'3%', display:'flex', flexWrap:'wrap', justifyContent:'space-between' }} className='middle-section'>
-            <div style={{ border:'1px solid black', height:'68%', width:'49%' }} className='restaurants-block'>
+            <div style={{ border:'1px solid black', height:'68%', width:'49%', borderRadius:'10px' }} className='restaurants-block'>
                 <Card style={{ width: '100%', height:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
                     <Card.Body>
                         <InputGroup style={{ margin:'2%', width:'90%'}} className="mb-3">
@@ -62,14 +62,23 @@ return (
                                     </Button>
                                     <Offcanvas show={show} onHide={handleClose}>
                                         <Offcanvas.Header closeButton>
-                                            <div></div>
-                                        <Offcanvas.Title>{profile && profile.likedrestaurants[0].name}</Offcanvas.Title>
-                                        <Card.Img  src={profile && profile.likedrestaurants[0].photos[2]} ></Card.Img>
-                                        </Offcanvas.Header>
-                                        <Offcanvas.Body>
-                                        Some text as placeholder. In real life you can have the elements you
-                                        have chosen. Like, text, images, lists, etc.
-                                        </Offcanvas.Body>
+                                            <div>
+                                            <Card style={{ width: '90%', margin:'0 auto' }}>
+                                                <Card.Img variant="top" src={profile && profile.likedrestaurants[0].photos[2]} />
+                                                <Card.Body>
+                                                    <Card.Title>{profile && profile.likedrestaurants[0].name}</Card.Title>
+                                                    <Card.Text> Some quick example text to build on the card title and make up the bulk of the card's content. </Card.Text>
+                                                </Card.Body>
+                                                <ListGroup className="list-group-flush">
+                                                    <ListGroup.Item><svg style={{ width:'20px' }} xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                                                    </svg>{profile && profile.likedrestaurants[0].location.address1}, {profile && profile.likedrestaurants[0].location.city}, {profile && profile.likedrestaurants[0].location.state}</ListGroup.Item>
+                                                    <ListGroup.Item>{profile && profile.likedrestaurants[0].display_phone}</ListGroup.Item>
+                                                    <ListGroup.Item>Type: {profile && profile.likedrestaurants[0].categories[0].title} <span style={{float:'right'}}>Price: {profile && profile.likedrestaurants[0].price}</span></ListGroup.Item>
+                                                </ListGroup>
+                                            </Card>
+                                            </div>
+                                            </Offcanvas.Header>
                                     </Offcanvas>
                                     </div>
                                     <Card.Body>
@@ -84,7 +93,7 @@ return (
                 </Card>
                 {/* LIKED RESTAURANTS COMPONENT  */}
             </div>
-            <div style={{ border:'1px solid black', height:'68%', width:'49%' }} className='friends-block'>
+            <div style={{ border:'1px solid black', height:'68%', width:'49%', borderRadius:'10px' }} className='friends-block'>
             <Card style={{ width: '100%', height:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
                     <Card.Body>
                         <InputGroup style={{ margin:'2%', width:'90%'}} className="mb-3">
@@ -111,7 +120,7 @@ return (
                 </Card>
                 {/* FRIEND LIST COMPONENT */}
             </div>
-            <div style={{ border:'1px solid black', height:'30%', width:'100%', marginTop:'2%'}} className='coordinate-block'>
+            <div style={{ border:'1px solid black', height:'30%', width:'100%', marginTop:'2%', borderRadius:'10px'}} className='coordinate-block'>
             <Card style={{ width: '100%', height:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
                     <Card.Body>
                         <Card.Title>Coordinate Meet 'n Eat with friends</Card.Title>
@@ -143,8 +152,10 @@ return (
                 {/* COORDINATE MEET UP COMPONENT */}
             </div>
         </div>
-        <div style={{ border:'1px solid black', width:'25%'}} className='itinerary-section'>
-
+        <div style={{ border:'1px solid black', width:'25%', borderRadius:'10px'}} className='itinerary-section'>
+            <div style={{ margin:'5%', display:'flex', justifyContent:'center' }}>
+                <h2 style={{ borderBottom:'1px solid rgb(211,211,211)' }}>Itinerary</h2>
+            </div>
         </div>
 
     </div>

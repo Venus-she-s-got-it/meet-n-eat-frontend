feffc036a4549fb9c10260f6b19bf91582e54555
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-06 18:22:01 -0700

    adds category and placeholder like button and rating stars in restaurantCard.js and adds searchString functionality and passes Search.js to searchResults.js

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
@@ -7,52 +7,69 @@ import Row from 'react-bootstrap/Row'
 
 
 const RestaurantCard = ({ restaurant }) => {
+    const [categories, setCategories] = useState()
     const { name, image_url, display_phone, price  } = restaurant
     const city = restaurant.location.city
     const state = restaurant.location.state
     const categoriesArr = []
+    for (let i=0; i < restaurant.categories.length; i++) {
+        categoriesArr.push(restaurant.categories[i].title)
+    }
     useEffect(() => {
-        for (let i=0; i < restaurant.categories[i].length; i++) {
-            categoriesArr.push(restaurant.categories[i].title)
-        }
+        setCategories(categoriesArr)
     }, [])
-if (!restaurant) {
-    return null
-}
-return (
-    <Container className="fluid">
-        <Card className ="py-1 px-1">
-            <Row>
-                <Col>
-                    <Container className="ml-1">
-                        <Image 
-                            src={image_url}
-                            alt="restaurant-image"
-                            width={200}
-                            height={200}
-                            />
-                        <Card.Title>{name}</Card.Title>
-                    </Container> 
-                </Col>
-                <Col>
-                    <Card.Body>
-                        <p>{price}</p>
-                        <p>M - F 9:00 AM - 8:00 PM</p>
-                        <Row>
-                            <Col>
-                                <p>{city}, {state}</p>
-                            </Col>
-                            <Col>
-                                <p>{display_phone}</p>
-                            </Col>
-                        </Row>
-                        <p>{categoriesArr}</p>
-                    </Card.Body>
-                </Col>
-            </Row>
-        </Card>
-    </Container>
-)
-}
+    
+if (categories) {
+    return (
+        <Container className="fluid">
+            <Card className ="py-1 px-1">
+                <Row>
+                    <Col>
+                        <Container className="ml-1">
+                            <Image 
+                                src={image_url}
+                                alt="restaurant-image"
+                                width={200}
+                                height={200}
+                                />
+                            <Card.Title>{name}</Card.Title>
+                        </Container> 
+                    </Col>
+                    <Col>
+                        <Card.Body>
+                            <Row>
+                                <Col>
+                                    {/* show results from other users who've liked restaurant */}
+                                    <p>Rating stars here</p>
+                                </Col>
+                                <Col>
+                                    {/* add onClick eventHandler to update users likedrestaurants */}
+                                    <button>Like Icon</button>
+                                </Col>
+                            </Row>
+                            <p>{price}</p>
+                            <p>M - F 9:00 AM - 8:00 PM</p>
+                            <Row>
+                                <Col>
+                                    <p>{city}, {state}</p>
+                                </Col>
+                                <Col>
+                                    <p>{display_phone}</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                            {categories.map(category => (
+                                <Col>
+                                <p>{category}</p>
+                                </Col>
+                            ))}
+                            </Row>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Card>
+        </Container>
+    )
+}}
 
 export default RestaurantCard

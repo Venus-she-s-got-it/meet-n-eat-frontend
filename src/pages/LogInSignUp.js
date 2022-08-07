edd6da194c5d8aa7724228bc3835545aff6d0248
hanswatkins <hanswatkins@gmail.com>
2022-08-07 14:12:30 -0700

    connected NavBar to logInSignUp page

--- src/pages/LogInSignUp.js
+++ src/pages/LogInSignUp.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import Login from '../components/LogIn';
 import SignUp from '../components/SignUp';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const LogInSignUp = () => {
-  const [loginSignup, setLoginSignup] = useState(false);
+  const { option } = useParams();
+  console.log(option);
+  const [loginSignup, setLoginSignup] = useState('');
+  useEffect(() => {
+    setLoginSignup(option);
+  }, [option]);
+
   return (
     <>
       <h1>MEET & EAT</h1>
 
-      <button onClick={() => setLoginSignup(!loginSignup)}>
+      <button onClick={() => setLoginSignup('login')}>
         <h2>LOG IN</h2>
       </button>
 
-      <button onClick={() => setLoginSignup(!loginSignup)}>
+      <button onClick={() => setLoginSignup('signup')}>
         <h2>SIGN UP</h2>
       </button>
 
-      {loginSignup === true && <Login />}
-      {loginSignup === false && <SignUp />}
+      {loginSignup === 'login' && <Login />}
+      {loginSignup === 'signup' && <SignUp />}
     </>
   );
 };

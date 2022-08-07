4e346cc00f56bbe41210ddd1ee1f3effc69b5676
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-07 15:44:48 -0700

    fetches user update url and builds state variables in ProfileSettings.js and builds reducer function

--- src/pages/ProfileSettings.js
+++ src/pages/ProfileSettings.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useReducer, useEffect, useContext } from 'react'
+import { UrlContext } from '../App'
+import axios from 'axios'
+import { userSettingsReducer } from '../data-and-functions/userSettingsReducer'
+import { Container, Card, Row, Col } from 'react-bootstrap'
 
 const ProfileSettings = () => {
+const url = useContext(UrlContext)
+// Initial State for userSettings
+const initialState = {
+    username: '',
+    profileimg: '',
+    about: '',
+    location: '',
+    displayname: '',
+    email: '',
+    likedrestaurants: ''
+}
+
+const [userSettings, dispatch] = useReducer(userSettingsReducer, initialState)
+
+// onChange or onSubmit  function
+function inputChange(e) {
+    dispatch({
+        key: 'profileSettings',
+        value: e.target.value
+    })
+}
+
+useEffect(() => {
+    axios.put(`${url}/users/62ed53ae80c5c665832c887d`, userSettings)
+    // axios.get(`${url}/restaurants/${searchString}`)
+        .then((res, err) => { 
+            if (res.status === 404) {
+                console.log(err)
+            } else if(res.status === 200 || res.status === 304) {
+                return res.data
+            }
+            console.log(userSettings)
+        })
+        // .then((data) => {
+            
+        // }) 
+        
+    }, [])
 
 return (
-    <div>Profile Settings</div>
+    <Container>
+        <Card>
+
+        </Card>
+    </Container>
 )
 }
 

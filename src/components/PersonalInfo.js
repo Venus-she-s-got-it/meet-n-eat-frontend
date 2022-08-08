becee19880d07b1a3b0509ed43682a805da5d9ca
hanswatkins <hanswatkins@gmail.com>
2022-08-08 15:05:35 -0700

    added 4 new components for FAQ sections. edited nav buttons to switch between the sections on FAQ.js

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PersonalInfo = () => {
+    return (
+        <div>
+            <ul>
+                <li>How do I create a new account?</li>
+                <li>Where can I update my profile picture?</li>
+                <li>What is a display name?</li>
+                <li>How do I change my username/email/display name?</li>
+                <li>How can I update my bio?</li>
+            </ul>
+        </div>
+    );
+};
+
+export default PersonalInfo;
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,27 +1,53 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import PersonalInfo from '../components/PersonalInfo';
+import Troubleshooting from '../components/Troubleshooting';
+import SearchQuestions from '../components/SearchQuestions';
+import UserInteractions from '../components/UserInteractions';
 
 const FAQ = () => {
+  // All static info?
+  const [activeElement, setActiveElement] = useState('personalinfo');
 
-// All static info?
+  return (
+    <div>
+      <div 
+        style={{
+        border: '1px solid black',
+        margin: '0 auto',
+        marginTop: '5%',
+        width: '50%',
+        height: '70vh',
+        }}>
+        
+        <Nav style={{ width: '100%', border: '1px solid black' }} variant='pills' defaultActiveKey='/faq'>
 
-return (
-    <div>FAQ Section
-        <div style={{ border:'1px solid black', margin:'0 auto', marginTop:'5%', width:'50%', height:'70vh' }}>
-            <Nav style={{ width:'100%', border:'1px solid black' }} variant="pills" defaultActiveKey="/faq">
-                <Nav.Item>
-                    <Nav.Link href="/faq">FAQ Section 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">FAQ Section 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2"> FAQ Section 3 </Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </div>
+            <Nav.Item>
+                <Nav.Link href="/faq" onClick={() => setActiveElement('personalinfo')}>Personal Info</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link eventKey='link-1' onClick={() => setActiveElement('troubleshooting')}>Troubleshooting</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link eventKey="link-2" onClick={() => setActiveElement('searchQuestions')}>Search Questions</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link eventKey="link-3" onClick={() => setActiveElement('userInteractions')}>User Interactions</Nav.Link>
+            </Nav.Item>
+        </Nav>
+
+        {activeElement === 'personalinfo' && <PersonalInfo />}
+        {activeElement === 'troubleshooting' && <Troubleshooting />}
+        {activeElement === 'searchQuestions' && <SearchQuestions />}
+        {activeElement === 'userInteractions' && <UserInteractions />}
+
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default FAQ  
+export default FAQ;

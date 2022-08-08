b965433e20966049bf8f0aca80fe5f534f97da0c
DavidJoao <davidsandoval596@gmail.com>
2022-08-08 10:54:58 -0700

    fixed bugs in profile page, working on itinerary/event functionality

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -2,11 +2,11 @@ import React from 'react'
 import MyProfile from '../pages/MyProfile'
 import { Card, ListGroup } from 'react-bootstrap'
 
-const ProfileCard = ( {username, location} ) => {
+const ProfileCard = ( {username, location, profileimg, about} ) => {
 
 return (
     <Card style={{ width: '90%', padding:'3%' }}>
-        <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+        <Card.Img variant="top" src={profileimg} />
         <Card.Body>
             <Card.Title>{username}</Card.Title>
                 {/* need to fix location */}
@@ -15,7 +15,7 @@ return (
         <ListGroup className="list-group-flush">
         <p style={{ textAlign:'center', fontWeight:'500' }}>About</p>
             <Card.Body>
-                <ListGroup.Item>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</ListGroup.Item>
+                <ListGroup.Item>{about}</ListGroup.Item>
             </Card.Body>
         </ListGroup>
     </Card>

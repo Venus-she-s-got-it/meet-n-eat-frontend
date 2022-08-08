7ee5becb03073904d12d6822e43e0dd471830283
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-07 17:54:26 -0700

    Separates message display into Message component
    Adds functionality for mapping over an array from MessageCenter
    Adds subject line, formats timestamp, and adds profile image from database
    
    Co-Authored-By: David Sandoval <82617473+DavidJoao@users.noreply.github.com>

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Card, ListGroup } from "react-bootstrap"
 
-const Message = () => {
+const Message = ({ message }) => {
+    const noImage = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    const dateObj = new Date(message.createdAt)
+    const date = dateObj.toLocaleDateString()
+    const time = dateObj.toLocaleTimeString("en", { timeStyle: 'short' })
 
 return (
-    <></>
+    <Card style={{ width: '70%',display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center', padding:'1%' }}>
+        <div style={{ width:'20%'}} >
+            <Card.Img style={{ width:'60%' }} variant="top" src={message.sender.profileimg || noImage } />
+            <Card.Text>{message && message.sender.username}</Card.Text>
+        </div>
+        <Card.Body>
+            <Card.Text>{message && `subject: ${message.subject}`}</Card.Text>
+            <Card.Text>{message && message.body}</Card.Text>
+        </Card.Body>
+            <Card.Footer>
+                <ListGroup.Item>{message && date}</ListGroup.Item>
+                <ListGroup.Item>{message && time}</ListGroup.Item>
+            </Card.Footer>
+    </Card>
 )
 }
 
--- src/pages/MessageCenter.js
+++ src/pages/MessageCenter.js
@@ -7,7 +7,8 @@ import { Button, Form, InputGroup, Card, ListGroup, ButtonGroup } from 'react-bo
 
 
 const MessageCenter = () => {
-
+// ID of currently logged in user (will be implemented later)
+const userId = '62ed53ae80c5c665832c887d'
 const url = useContext(UrlContext)
 
 //messages
@@ -16,50 +17,35 @@ const [messages, setMessages] = useState(null)
 const [requests, setRequests] = useState(null)
 
 useEffect ( () => {
-    axios.get('http://localhost:8000/messages')
+    // Get messages
+    axios.get(`${url}/messages`)
     .then((res) => {
         setMessages(res.data)
-        console.log(res.data)
-        
+        console.log("- messages", res.data)
     })
     
-    // need a requests property? 
-    axios.get('http://localhost:8000/users', requests)
-    .then((res) => {
-        // setRequests(res.data.requests??)
-    })
+    // Get friend requests
+    // axios.get(`${url}/users/${userId}`)
+    // .then((res) => {
+    //     setRequests(res.data)
+    //     console.log("- friend requests", res.data)
+    // })
 },[]) 
-
-// messages[0].sender.username
-
-// messages[0].body
 
 
 return (
     <div style={{ border:'1px solid black', margin:'2%', height:'90vh', display:'flex', flexDirection:'row' }} className="message-center">
         <div style={{ border:'1px solid black', width:'55%', margin:'2%', display:'flex', flexDirection:'column', alignItems:'center'}} className='message-section'>
             <InputGroup style={{ margin:'2%', width:'60%' }} className="mb-3">
-                <Form.Control placeholder="Search in messages" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                <Form.Control placeholder="Search messages" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
                 <Button variant="outline-secondary" id="button-addon2"> Search </Button>
             </InputGroup>
             <div style={{ display:'flex', flexDirection:'column', alignItems:'center' }} className='messages-area'>
-                <Card style={{ width: '70%',display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center', padding:'1%' }}>
-                    <div style={{ width:'20%'}} >      
-                    <Card.Img style={{ width:'60%' }} variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
-                    <Card.Text>{messages && messages[0].sender.username}</Card.Text>
-                    </div>
-                    <Card.Body>
-                        <Card.Text>{messages && messages[0].body}</Card.Text>
-                    </Card.Body>
-                        <ListGroup.Item>{messages && messages[0].updatedAt}</ListGroup.Item>
-                </Card>
-            </div>
-                <div>
-                    {/* {messages && messages.map((message) => <Message message={message} key={message._id}/>)} */}
-                </div>      
+                {messages && messages.map((message) => <Message message={message} key={message._id}/>)} 
+            </div>     
         </div>
         <div style={{ border:'1px solid black', margin:'2%', width:'35%', display:'flex', flexDirection:'column', alignItems:'center'}} className='requests-section'>
-        <Card style={{ width: '70%',display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center', padding:'1%' }}>
+        <Card className='d-flex flex-row justify-content-center align-items-center' style={{ width: '70%', padding:'1%' }}>
                     <div style={{ width:'20%'}} >      
                     <Card.Img style={{ width:'60%' }} variant="top" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
                     <Card.Text>{messages && messages[0].recipients[0].username}</Card.Text>

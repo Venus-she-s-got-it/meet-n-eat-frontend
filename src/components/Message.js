801d91067f55e1603fb54ca995abde04ee95c213
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-08-08 10:27:48 -0700

    Adds display code to FriendRequest
    Adds formatDateTime function for formatting timestamps.
    Adds clickHandler function for accept/decline buttons with logic for database calls based on user choice.
    
    Co-Authored-By: David Sandoval <82617473+DavidJoao@users.noreply.github.com>

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,16 +1,14 @@
 import { Card, ListGroup } from "react-bootstrap"
+import { formatDateTime } from "../data-and-functions/formatDateTime"
 
-const Message = ({ message }) => {
-    const noImage = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-    const dateObj = new Date(message.createdAt)
-    const date = dateObj.toLocaleDateString()
-    const time = dateObj.toLocaleTimeString("en", { timeStyle: 'short' })
+const Message = ({ message, noImage }) => {
+    const [date, time] = formatDateTime(message.createdAt)
 
 return (
     <Card style={{ width: '70%',display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center', padding:'1%' }}>
         <div style={{ width:'20%'}} >
             <Card.Img style={{ width:'60%' }} variant="top" src={message.sender.profileimg || noImage } />
-            <Card.Text>{message && message.sender.username}</Card.Text>
+            <Card.Text>{message && message.sender.displayname}</Card.Text>
         </div>
         <Card.Body>
             <Card.Text>{message && `subject: ${message.subject}`}</Card.Text>

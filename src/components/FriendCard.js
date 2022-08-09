7e840f9132eedc641461d8567064680ee07fcd43
hanswatkins <hanswatkins@gmail.com>
2022-08-09 16:21:06 -0700

    finished basic styling of friendcard

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -1,25 +1,42 @@
-import { useContext } from "react"
-import { Card, Row, Col } from "react-bootstrap"
-import { UrlContext } from "../App"
+import { useContext } from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
+import { UrlContext } from '../App';
 
 const FriendCard = ({ friend }) => {
-const { defaultImage } = useContext(UrlContext)
+  const { defaultImage } = useContext(UrlContext);
 
-return (
-    <Card style={{ width: '90%', padding:'3%', marginBottom:'5%'}}>
-        <Row>
-            <Col>
-                <Card.Img variant="top" src={friend.profileimg || defaultImage} />
-            </Col>
-            <Col>
-                <Card.Body>
-                    <Card.Title>{friend.username}</Card.Title>
-                    <Card.Text>  {friend.location} </Card.Text>
-                </Card.Body>
-            </Col>
-        </Row>
+  return (
+    <Card
+      style={{
+        border: '1px solid #EB3510',
+        width: '90%',
+        padding: '3%',
+        marginBottom: '5%',
+      }}
+    >
+      <Row>
+        <Col>
+          <Card.Img
+            style={{
+              border: '1px solid #EB3510',
+              borderRadius: '100%',
+            }}
+            variant='top'
+            src={friend.profileimg || defaultImage}
+          />
+        </Col>
+        <Col>
+          <Card.Body>
+            <Card.Title>{friend.username}</Card.Title>
+            <Card.Text style={{ fontSize: '0.8rem' }}>
+              {' '}
+              {friend.location}{' '}
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
-)
-}
+  );
+};
 
-export default FriendCard
+export default FriendCard;

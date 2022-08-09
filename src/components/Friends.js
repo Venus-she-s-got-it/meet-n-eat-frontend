b6fbf8f0031773c840b602d01d6e514811fe3131
DavidJoao <davidsandoval596@gmail.com>
2022-08-08 21:58:18 -0700

    adds functionality to search filters on liked restaurants and friends

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { Card, InputGroup, Form, Button, ListGroup } from 'react-bootstrap'
 import FriendCard from './FriendCard'
+import { useState } from 'react'
 
 const Friends = ( {profileExample} ) => {
 
     const friends = profileExample[0].friends;
+
+    const [searchCharacters, setSearchCharacters] = useState('')
+
 return (
     <Card style={{ width: '100%', height:'100%', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
     <Card.Body>
             <Card.Body style={{width:'100%', heigth:'70%'}}>
             <div>
                 <InputGroup style={{ margin:'2%', width:'90%'}} className="mb-3">
-                <Form.Control placeholder="Search Friends" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                <Form.Control onChange={e => {
+                    setSearchCharacters(e.target.value
+                        )}
+                    } type={true} placeholder="Search Friends" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
                 <Button variant="outline-secondary" id="button-addon2"> Search </Button>
                 </InputGroup>
-                {friends && friends.map(friend =>  <FriendCard key={friend._id} friend={friend} />)}
+                {friends && friends.filter((friend) => {
+                    if (searchCharacters == ''){
+                        return friend
+                    } else if (friend.username.toLowerCase().includes(searchCharacters.toLocaleLowerCase())) {
+                        return friend
+                    }   
+                }).map(friend =>  <FriendCard key={friend._id} friend={friend} />)}
             </div>
             </Card.Body>
         </Card.Body>

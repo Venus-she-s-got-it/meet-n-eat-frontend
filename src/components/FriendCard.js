3d101be3fdcc1218fd538a328f919427bd300250
hanswatkins <hanswatkins@gmail.com>
2022-08-09 13:49:03 -0700

    Added margin to FriendCard

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -6,7 +6,7 @@ const FriendCard = ({ friend }) => {
 const { defaultImage } = useContext(UrlContext)
 
 return (
-    <Card style={{ width: '90%', padding:'3%' }}>
+    <Card style={{ width: '90%', padding:'3%', marginBottom:'5%'}}>
         <Row>
             <Col>
                 <Card.Img variant="top" src={friend.profileimg || defaultImage} />
@@ -14,7 +14,7 @@ return (
             <Col>
                 <Card.Body>
                     <Card.Title>{friend.username}</Card.Title>
-                    <Card.Text> {friend.location} </Card.Text>
+                    <Card.Text>  {friend.location} </Card.Text>
                 </Card.Body>
             </Col>
         </Row>
